83816d8f9a4b83f2b7f78642de672e21486c2587
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-07-11 22:21:28 -0300

    🐕 work in progress: return message to client

--- backend/src/network/websocket.rs
+++ backend/src/network/websocket.rs
@@ -1,6 +1,8 @@
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
 use futures_util::stream::StreamExt;
+use tokio_tungstenite::tungstenite::Message;
+use futures_util::sink::SinkExt;
 
 
 #[tokio::main]
@@ -17,8 +19,52 @@ async fn accept_connection(stream: tokio::net::TcpStream) {
 
     println!("New WebSocket connection: {}", ws_stream.get_ref().peer_addr().unwrap());
 
-    let (write, read) = ws_stream.split();
+    let (mut write, read) = ws_stream.split();
 
-    read.forward(write).await.unwrap();
+    // Send a response back to the client
+    // read.forward(write).await;
+
+    read.for_each(|result| {
+        match result {
+            Ok(message) => {
+                let msg = &message; // Todo: I did this with some sort of luck. Please study more
+                                    // about Rust Ownership and Variables Scope. This do not look
+                                    // like a good pattern to keep.
+                let _ = write.send(Message::Text(msg.clone().to_string()));
+                handle_message(msg.clone());
+            }
+            Err(err) => {
+                eprintln!("error receiving message: {}", err);
+            }
+        }
+        futures::future::ready(())
+    }).await;
+
 }
 
+
+fn handle_message(message: Message) {
+    match message {
+        Message::Text(text) => {
+            println!("Received text message: {}", text);
+            // Handle the text message here
+        }
+        Message::Binary(data) => {
+            println!("Received binary message with {} bytes", data.len());
+            // Handle the binary message here
+        }
+        Message::Ping(_) => {
+            // Handle the ping message here
+        }
+        Message::Pong(_) => {
+            // Handle the pong message here
+        }
+        Message::Close(_) => {
+            // Handle the close message here
+            println!("WebSocket connection closed");
+        }
+        Message::Frame(_) => {
+
+        }
+    }
+}
