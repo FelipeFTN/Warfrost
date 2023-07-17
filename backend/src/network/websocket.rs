532328c894c7f00cdb2e45d754afc5e183ae2185
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-07-17 19:47:02 -0300

    🦀 refactor websocket

--- backend/src/network/websocket.rs
+++ backend/src/network/websocket.rs
@@ -1,70 +1,36 @@
-use tokio::net::TcpListener;
-use tokio_tungstenite::accept_async;
-use futures_util::stream::StreamExt;
-use tokio_tungstenite::tungstenite::Message;
-use futures_util::sink::SinkExt;
+use simple_websockets::{Event, Responder, Message};
+use std::collections::HashMap;
 
+pub fn start() {
+    // Listen for WebSockets on port 8080:
+    let event_hub = simple_websockets::launch(8080)
+        .expect("failed to listen on port 8080.");
+    
+    // map between client ids & client's 'Responder':
+    let mut clients: HashMap<u64, Responder> = HashMap::new();
 
-#[tokio::main]
-pub async fn start() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
-    }
-}
-
-async fn accept_connection(stream: tokio::net::TcpStream) {
-    let ws_stream = accept_async(stream).await.unwrap();
-
-    println!("New WebSocket connection: {}", ws_stream.get_ref().peer_addr().unwrap());
-
-    let (mut write, read) = ws_stream.split();
-
-    // Send a response back to the client
-    // read.forward(write).await;
-
-    read.for_each(|result| {
-        match result {
-            Ok(message) => {
-                let msg = &message; // Todo: I did this with some sort of luck. Please study more
-                                    // about Rust Ownership and Variables Scope. This do not look
-                                    // like a good pattern to keep.
-                let _ = write.send(Message::Text(msg.clone().to_string()));
-                handle_message(msg.clone());
-            }
-            Err(err) => {
-                eprintln!("error receiving message: {}", err);
-            }
-        }
-        futures::future::ready(())
-    }).await;
-
-}
-
-
-fn handle_message(message: Message) {
-    match message {
-        Message::Text(text) => {
-            println!("Received text message: {}", text);
-            // Handle the text message here
-        }
-        Message::Binary(data) => {
-            println!("Received binary message with {} bytes", data.len());
-            // Handle the binary message here
-        }
-        Message::Ping(_) => {
-            // Handle the ping message here
-        }
-        Message::Pong(_) => {
-            // Handle the pong message here
-        }
-        Message::Close(_) => {
-            // Handle the close message here
-            println!("WebSocket connection closed");
-        }
-        Message::Frame(_) => {
-
-        }
+    // Work in Progress: Create a separeted Handler
+    loop {
+        match event_hub.poll_event() {
+            Event::Connect(client_id, responder) => {
+                println!("A client connected with id #{}.", client_id);
+                // add their Responser to our 'clients' map:
+                clients.insert(client_id, responder);
+            },
+            Event::Disconnect(client_id) => {
+                println!("A client with id #{} disconnected.", client_id);
+                // add their Responser to our 'clients' map:
+                clients.remove(&client_id);
+            },
+            Event::Message(client_id, message) => {
+                if let Message::Text(text) = &message {
+                    println!("Received a message from client #{}: {}", client_id, text);
+                } 
+                // retrieve this client's 'Responder':
+                let responder = clients.get(&client_id).unwrap();
+                // echo the message back;
+                responder.send(message);
+            },
+        }    
     }
 }
