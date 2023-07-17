c6275eb47c3b956aec0699f956ea03792fd02aa1
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-07-17 19:57:47 -0300

    🐶 docker scripts

--- docker/frontend-init.sh
+++ docker/frontend-init.sh
@@ -5,4 +5,4 @@ cd ../frontend
 docker buildx build -t warfrost-frontend .
 
 # Run Container
-docker run -itd -p 80:80 --net=warfrost-network --ip 172.20.0.3 --name container-warfrost-frontend warfrost-frontend
+docker run -itd -p 80:80 --net warfrost-network --ip 172.20.0.3 --name container-warfrost-frontend warfrost-frontend
