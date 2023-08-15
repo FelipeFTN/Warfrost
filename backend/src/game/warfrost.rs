bf8699c1de61519dc89940cf8bdc2b763f88b90e
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-08-15 14:53:31 -0300

    📤 expose clients

--- backend/src/game/warfrost.rs
+++ backend/src/game/warfrost.rs
@@ -1,8 +1,11 @@
+use simple_websockets::Responder;
+use std::collections::HashMap;
 use super::players::Players;
 
 use crate::network::websocket::network;
 
 pub fn warfrost() {
     let mut players = Players::new();
-    network(&mut players);
+    let mut clients: HashMap<u64, Responder> = HashMap::new();
+    network(&mut clients, &mut players);
 }
--- backend/src/network/websocket.rs
+++ backend/src/network/websocket.rs
@@ -4,24 +4,22 @@ use std::collections::HashMap;
 use crate::network::handlers::{ws_connect, ws_disconnect, ws_message};
 use crate::game::players::Players;
 
-pub fn network(players: &mut Players) {
+pub fn network(clients: &mut HashMap<u64, Responder>, players: &mut Players) {
     // Listen for WebSockets on port 8080
     // TODO: Get the port from environment variable
     let event_hub = simple_websockets::launch(8080).expect("failed to listen on port 8080.");
 
     // map between client ids & client's 'Responder'
-    let mut clients: HashMap<u64, Responder> = HashMap::new();
-
     loop {
         match event_hub.poll_event() {
             Event::Connect(client_id, responder) => {
-                ws_connect(client_id, &mut clients, players, responder);
+                ws_connect(client_id, clients, players, responder);
             }
             Event::Disconnect(client_id) => {
-                ws_disconnect(client_id, &mut clients, players);
+                ws_disconnect(client_id, clients, players);
             }
             Event::Message(client_id, message) => {
-                ws_message(client_id, message, &mut clients, players);
+                ws_message(client_id, message, clients, players);
             }
         }
     }
